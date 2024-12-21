aa101faa7ae3dfe92dcf4862183e423ee0263854
kdu2030 <kdu2030@gmail.com>
2024-12-21 17:57:35 -0500

    Add Jwt Configuration

---
+++ PrayerAppServices/Configuration/JwtConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace PrayerAppServices.Configuration {
+    public static class JwtConfiguration {
+        public static void AddJwtConfiguration(this IServiceCollection services, IConfiguration configuration) {
+            string? jwtKey = configuration["Jwt:Key"];
+            if (jwtKey == null) {
+                throw new ArgumentNullException(nameof(jwtKey));
+            }
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        ValidAudience = configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                    };
+                });
+
+            services.AddSingleton<JwtSecurityTokenHandler>();
+
+        }
+    }
+}
--- PrayerAppServices/Program.cs
+++ PrayerAppServices/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext(configuration);
+builder.Services.AddJwtConfiguration(configuration);
 
 var app = builder.Build();
 
