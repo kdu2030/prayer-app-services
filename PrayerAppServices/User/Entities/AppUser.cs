8aca6cc2add8ee008750145db910f30832480256
kdu2030 <kdu2030@gmail.com>
2024-12-21 17:50:36 -0500

    Call base constructor in AppUser default Constructor

--- PrayerAppServices/User/Entities/AppUser.cs
+++ PrayerAppServices/User/Entities/AppUser.cs
@@ -6,7 +6,8 @@ namespace PrayerAppServices.User.Entities {
         [Column(TypeName = "varchar(256)")]
         public string? FullName { get; set; }
 
-        public AppUser() { }
+        public AppUser() : base() {
+        }
 
         public AppUser(string username, string fullName, string email, string passwordHash) : base(username) {
             FullName = fullName;
