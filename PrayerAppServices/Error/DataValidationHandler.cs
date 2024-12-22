65c9eb19551a20c24e35e6818118fcfaefebc1b3
kdu2030 <kdu2030@gmail.com>
2024-12-21 19:36:30 -0500

    Add Handle Data Validation Errors

---
+++ PrayerAppServices/Error/DataValidationHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrayerAppServices.Error {
+    public class DataValidationHandler {
+        public static IActionResult HandleDataValidationErrors(ActionContext context) {
+            IEnumerable<string> errorMessages = context.ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage);
+            var error = new DataValidationError {
+                ErrorCode = ErrorCode.DataValidationError,
+                DataValidationErrors = errorMessages,
+                Message = "Data validation error",
+                Url = context.HttpContext.Request.Path,
+                RequestMethod = context.HttpContext.Request.Method
+            };
+            return new BadRequestObjectResult(error);
+        }
+    }
+}
