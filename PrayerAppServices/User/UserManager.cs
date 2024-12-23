0165eda5f3208961962acb01c9ef91f89dc58641
kdu2030 <kdu2030@gmail.com>
2024-12-23 11:51:20 -0500

    Add Issuer and Audience

--- PrayerAppServices/User/UserManager.cs
+++ PrayerAppServices/User/UserManager.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using PrayerAppServices.User.Entities;
 using PrayerAppServices.User.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace PrayerAppServices.User {
-    public class UserManager(UserManager<AppUser> userManager) {
+    public class UserManager(UserManager<AppUser> userManager, IConfiguration configuration, JwtSecurityTokenHandler jwtSecurityTokenHandler) : IUserManager {
         private UserManager<AppUser> _userManager = userManager;
+        private IConfiguration _configuration = configuration;
+        private JwtSecurityTokenHandler _jwtSecurityTokenHandler = jwtSecurityTokenHandler;
 
         private const int AccessTokenValidityMs = 60 * 60 * 1000;
         private const int RefreshTokenValidityMs = 15 * 24 * 60 * 60 * 1000;
@@ -39,6 +44,23 @@ namespace PrayerAppServices.User {
             return Convert.ToHexString(hashBytes);
         }
 
+        private string GenerateToken(AppUser user) {
+            string? jwtKey = _configuration["Jwt:Key"];
+            string? issuer = _configuration["Jwt:Issuer"];
+            string? audience = _configuration["Jwt:Audience"];
+
+            if (jwtKey == null || issuer == null || audience == null) {
+                throw new InvalidOperationException("Jwt:Key, Jwt:Issuer, and Jwt:Audience must be set in configuration.");
+            }
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            Claim[] claims = [
+                new Claim(ClaimTypes.Name, user.UserName ?? "")
+            ];
+
+            throw new NotImplementedException();
+        }
 
     }
 }
