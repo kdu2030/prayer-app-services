a75e3eae9f520a170df91797a70702df2af1dbd9
kdu2030 <kdu2030@gmail.com>
2024-12-25 11:10:53 -0500

    Clean up Test Services Setup

---
+++ Tests/TestConfiguration.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PrayerAppServices.Configuration;
+using PrayerAppServices.Data;
+using PrayerAppServices.User.Entities;
+
+namespace Tests {
+    public static class TestConfiguration {
+        private static readonly Dictionary<string, string> JwtConfiguration = new Dictionary<
+           string,
+           string
+       >
+       {
+            { "Jwt:Key", "459b5631306a7ec4573e52e83bcc1a22ce2e19682f5e31bb965a9709f1fd99c1" },
+            { "Jwt:Issuer", "http://localhost:5152/" },
+            { "Jwt:Audience", "http://localhost:5152/" },
+        };
+
+        public static void AddTestServices(this IServiceCollection services) {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(JwtConfiguration)
+                .Build();
+
+            services.AddSingleton(configuration);
+            services.AddJwtConfiguration(configuration);
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("TestDatabase"));
+            services.AddIdentityApiEndpoints<AppUser>()
+                .AddEntityFrameworkStores<AppDbContext>();
+        }
+    }
+}
--- Tests/UserManagerTests.cs
+++ Tests/UserManagerTests.cs
@@ -1,40 +1,17 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PrayerAppServices.Configuration;
 using PrayerAppServices.Data;
 using PrayerAppServices.User;
-using PrayerAppServices.User.Entities;
 using PrayerAppServices.User.Models;
 
 namespace Tests {
     public class UserManagerTests {
         private ServiceProvider _serviceProvider;
-        private static readonly Dictionary<string, string> JwtConfiguration = new Dictionary<
-            string,
-            string
-        >
-        {
-            { "Jwt:Key", "459b5631306a7ec4573e52e83bcc1a22ce2e19682f5e31bb965a9709f1fd99c1" },
-            { "Jwt:Issuer", "http://localhost:5152/" },
-            { "Jwt:Audience", "http://localhost:5152/" },
-        };
 
         [SetUp]
         public void Setup() {
-            // TODO: Need to add an IConfiguration to the service collection
-            // Use in memory configuration
-            IConfiguration configuration = new ConfigurationBuilder()
-                .AddInMemoryCollection(JwtConfiguration)
-                .Build();
-
             IServiceCollection services = new ServiceCollection();
-            services.AddSingleton(configuration);
-            services.AddJwtConfiguration(configuration);
-            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("TestDatabase"));
+            services.AddTestServices();
             services.AddTransient<IUserManager, UserManager>();
-            services.AddIdentityApiEndpoints<AppUser>()
-                .AddEntityFrameworkStores<AppDbContext>();
             _serviceProvider = services.BuildServiceProvider();
         }
 
