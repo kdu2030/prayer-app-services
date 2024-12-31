9507f57b2b524ab71a717aa6088413e796798f5d
kdu2030 <kdu2030@gmail.com>
2024-12-31 15:14:29 -0500

    Add RestSharp Library and Add Rest Client configuration

--- PrayerAppServices/Files/FileManager.cs
+++ PrayerAppServices/Files/FileManager.cs
@@ -1,13 +1,26 @@
 ﻿using PrayerAppServices.Files.Constants;
 using PrayerAppServices.Files.Entities;
+using RestSharp;
 
 namespace PrayerAppServices.Files {
-    public class FileManager : IFileManager {
+    public class FileManager(IConfiguration configuration) : IFileManager {
+        private readonly IConfiguration _configuration = configuration;
+
         public async Task<MediaFileBase> UploadFileAsync(IFormFile file) {
             FileType fileType = MediaFile.GetFileTypeFromContentType(file.ContentType);
+
             if (fileType == FileType.Unknown) {
                 throw new ArgumentException("Unsupported file type");
             }
+
+            string fileName = file.FileName;
+            string fileServicesUrl = _configuration["FileUpload:Url"]
+                ?? throw new NullReferenceException("File Upload URL cannot be null.");
+
+            RestClient restClient = new RestClient(fileServicesUrl);
+            RestRequest restRequest = new RestRequest("/file", Method.Post);
+
+            Console.WriteLine(fileName);
             throw new NotImplementedException();
         }
     }
