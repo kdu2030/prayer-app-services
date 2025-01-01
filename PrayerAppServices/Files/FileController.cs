81dbbbb7a53dfed3bba9d748b2b5a860d86a7f88
kdu2030 <kdu2030@gmail.com>
2024-12-31 20:24:34 -0500

    Lock Down Upload File Async

--- PrayerAppServices/Files/FileController.cs
+++ PrayerAppServices/Files/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PrayerAppServices.Files.Entities;
 using System.Net;
 
@@ -10,6 +11,7 @@ namespace PrayerAppServices.Files {
         private readonly IFileManager _fileManager = fileManager;
 
         [HttpPost]
+        [Authorize]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(MediaFileBase))]
         public async Task<IActionResult> UploadFileAsync(IFormFile file) {
             MediaFileBase mediaFile = await _fileManager.UploadFileAsync(file);
