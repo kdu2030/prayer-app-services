05e88ee324528d4c4fa25e9bcb1ae66c5c1531c5
kdu2030 <kdu2030@gmail.com>
2024-12-31 19:16:18 -0500

    Add Test Setup for File Manager

--- Tests/FileManagerTests.cs
+++ Tests/FileManagerTests.cs
@@ -1,15 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using PrayerAppServices.Files;
+using PrayerAppServices.Files.Models;
+using RestSharp;
 
 namespace Tests {
     public class FileManagerTests {
         private ServiceProvider _serviceProvider;
+        private Mock<IRestClient> _mockRestClient;
 
         [SetUp]
         public void SetUp() {
             IServiceCollection services = new ServiceCollection();
             services.AddTestServices();
             services.AddTransient<IFileManager, FileManager>();
+            services.AddTransient(options => _mockRestClient.Object);
             _serviceProvider = services.BuildServiceProvider();
         }
 
@@ -20,6 +25,16 @@ namespace Tests {
         }
 
         [Test]
+        public void UploadFileAsync_WhenGivenValidFile_ReturnsMediaFile() {
+            RestRequest mockRequest = new RestRequest("/file", Method.Post);
 
+            RestResponse<FileUploadResponse> mockResponse = new RestResponse<FileUploadResponse>(mockRequest);
+            mockResponse.Data = new FileUploadResponse { IsError = false, Url = "" };
+
+            _mockRestClient.Setup(_mockRestClient =>
+                _mockRestClient.ExecuteAsync<FileUploadResponse>(It.IsAny<RestRequest>(), new CancellationToken()))
+            .ReturnsAsync(() => mockResponse);
+
+        }
     }
 }
