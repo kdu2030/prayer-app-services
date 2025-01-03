8d9841f8dcaaf594a262e646f8f31da32c3e54b5
kdu2030 <kdu2030@gmail.com>
2025-01-03 08:25:03 -0500

    Add Draft Test for Valid Delete

--- Tests/FileManagerTests.cs
+++ Tests/FileManagerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using PrayerAppServices.Data;
 using PrayerAppServices.Files;
 using PrayerAppServices.Files.Constants;
 using PrayerAppServices.Files.Entities;
@@ -16,7 +17,7 @@ namespace Tests {
         [SetUp]
         public void SetUp() {
             IServiceCollection services = new ServiceCollection();
-            _mockFileServicesClient = new Mock<IFileServicesClient>();
+            _mockFileServicesClient = new Mock<IFileServicesClient>() { CallBase = true };
 
             services.AddTestServices();
             services.AddTransient<IMediaFileRepository, MediaFileRepository>();
@@ -65,6 +66,45 @@ namespace Tests {
             IFormFile file = CreateTestFormFile("test.txt", "text/plain", "test content");
             IFileManager fileManager = scope.ServiceProvider.GetRequiredService<IFileManager>();
             Assert.ThrowsAsync<ArgumentException>(() => fileManager.UploadFileAsync(file));
+        }
+
+        [Test]
+        public async Task DeleteFileAsync_GivenValidFileId_DeletesFile() {
+            IServiceCollection services = new ServiceCollection();
+            services.AddTestServices();
+
+            RestRequest fileDeleteRequest = new RestRequest("/file/1.png", Method.Delete);
+            RestResponse<FileDeleteResponse> fileDeleteResponse = new RestResponse<FileDeleteResponse>(fileDeleteRequest) {
+                StatusCode = System.Net.HttpStatusCode.OK,
+                IsSuccessStatusCode = true,
+                ResponseStatus = ResponseStatus.Completed
+            };
+
+            Mock<IMediaFileRepository> mockFileRepository = new Mock<IMediaFileRepository>() { CallBase = true };
+            mockFileRepository.Setup(mockFileRepository => mockFileRepository.ValidateMediaFileDelete(It.IsAny<int>()))
+                .Returns(new List<FileDeleteError>());
+            _mockFileServicesClient.Setup(_mockServicesClient => _mockServicesClient.ExecuteAsync<FileDeleteResponse>(It.IsAny<RestRequest>())).ReturnsAsync(
+                fileDeleteResponse);
+
+            services.AddTransient(options => mockFileRepository.Object);
+            services.AddTransient<IFileManager, FileManager>();
+            services.AddTransient(options => _mockFileServicesClient.Object);
+
+            _serviceProvider = services.BuildServiceProvider();
+
+            using IServiceScope scope = _serviceProvider.CreateScope();
+            MediaFile file = new MediaFile { Id = 1, Name = "leslieknope.png", Type = FileType.Image, Url = "http://localhost:5000/static/2.png" };
+
+            AppDbContext appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            appDbContext.MediaFiles.Add(file);
+            appDbContext.SaveChanges();
+
+            IFileManager fileManager = scope.ServiceProvider.GetRequiredService<IFileManager>();
+            await fileManager.DeleteFileAsync(file.Id ?? -1);
+
+            MediaFile? mediaFile = appDbContext.MediaFiles.Find(file.Id);
+            Assert.That(mediaFile, Is.Null);
         }
 
         private IFormFile CreateTestFormFile(string fileName, string contentType, string content) {
