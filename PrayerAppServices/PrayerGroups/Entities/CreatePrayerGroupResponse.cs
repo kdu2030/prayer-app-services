c5df110b5708d840c14d8218d8b6c6e2b4f91e82
kdu2030 <kdu2030@gmail.com>
2025-01-05 09:01:06 -0500

    Add CreatePrayerGroupResponse Entity

--- PrayerAppServices/PrayerGroups/Entities/CreatePrayerGroupResponse.cs
+++ PrayerAppServices/PrayerGroups/Entities/CreatePrayerGroupResponse.cs
@@ -1,7 +1,17 @@
 ﻿namespace PrayerAppServices.PrayerGroups.Entities {
     public class CreatePrayerGroupResponse {
-        public required int AdminUserId { get; set; }
-        public string? AdminUserName { get; set; }
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Rules { get; set; }
+        public int? Color { get; set; }
+        public int? ImageFileId { get; set; }
+        public string? GroupImageFileName { get; set; }
+        public string? GroupImageFileUrl { get; set; }
+        public int? AdminUserId { get; set; }
+        public string? AdminFullName { get; set; }
         public int? AdminImageFileId { get; set; }
+        public string? AdminImageFileName { get; set; }
+        public string? AdminImageFileUrl { get; set; }
     }
 }
