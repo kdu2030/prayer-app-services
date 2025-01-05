b218e9b73b615df2f8c9a497cacd764245346be0
kdu2030 <kdu2030@gmail.com>
2025-01-05 14:21:27 -0500

    Add Create Prayer Group Method

---
+++ PrayerAppServices/PrayerGroups/Models/NewPrayerGroupResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace PrayerAppServices.PrayerGroups.Models {
+    public class NewPrayerGroupResponse {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Rules { get; set; }
+        public string? Color { get; set; }
+        public int? ImageFileId { get; set; }
+    }
+}
--- PrayerAppServices/PrayerGroups/PrayerGroupRepository.cs
+++ PrayerAppServices/PrayerGroups/PrayerGroupRepository.cs
@@ -1,5 +1,7 @@
-﻿using PrayerAppServices.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using PrayerAppServices.Data;
 using PrayerAppServices.PrayerGroups.Entities;
+using PrayerAppServices.PrayerGroups.Models;
 
 namespace PrayerAppServices.PrayerGroups {
     public class PrayerGroupRepository(AppDbContext dbContext) : IPrayerGroupRepository {
@@ -9,6 +11,20 @@ namespace PrayerAppServices.PrayerGroups {
             _dbContext.PrayerGroups.Add(prayerGroup);
             await _dbContext.SaveChangesAsync();
             return prayerGroup;
+        }
+
+        public CreatePrayerGroupResponse CreatePrayerGroupAsync(string adminUsername, NewPrayerGroup newPrayerGroup) {
+            FormattableString sqlQuery = $@"SELECT * FROM create_prayer_group(
+                ${adminUsername},
+                ${newPrayerGroup.Name},
+                ${newPrayerGroup.Description},
+                ${newPrayerGroup.Rules},
+                ${newPrayerGroup.Color},
+                ${newPrayerGroup.ImageFileId}
+            );";
+            return _dbContext.Database.SqlQuery<CreatePrayerGroupResponse>(
+               sqlQuery
+            ).First();
         }
     }
 }
