339dedd9c08d35716e4ac72ca8ccc105c1d8e737
kdu2030 <kdu2030@gmail.com>
2025-01-06 21:03:26 -0500

    Add Method to Interface

--- PrayerAppServices/PrayerGroups/IPrayerGroupRepository.cs
+++ PrayerAppServices/PrayerGroups/IPrayerGroupRepository.cs
@@ -4,7 +4,6 @@ using PrayerAppServices.PrayerGroups.Models;
 namespace PrayerAppServices.PrayerGroups {
     public interface IPrayerGroupRepository {
         CreatePrayerGroupResponse CreatePrayerGroup(string adminUsername, NewPrayerGroup newPrayerGroup);
-
         Task<PrayerGroup?> GetPrayerGroupByIdAsync(int id);
     }
 }
