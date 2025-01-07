8394367b08cc65c394cc74d7a010ce325b0bbbbf
kdu2030 <kdu2030@gmail.com>
2025-01-07 11:14:22 -0500

    Add Get Prayer Group Admins

---
+++ PrayerAppServices/SQL/get_prayer_group_admins.sql
@@ -0,0 +1,34 @@
+﻿CREATE OR REPLACE FUNCTION get_prayer_group_admins(group_id INT)
+RETURNS TABLE (
+    id INT,
+    full_name VARCHAR(255),
+    group_role INT,
+    image_file_id INT,
+    file_name VARCHAR(255),
+    file_url VARCHAR(255),
+    file_type INT
+) 
+AS
+$$
+BEGIN
+    RETURN QUERY
+    SELECT
+        a.id,
+        a.full_name,
+        g.role,
+        a.image_file_id,
+        f.name,
+        f.url,
+        f.type
+    FROM 
+        prayer_group_users g 
+    INNER JOIN 
+        asp_net_users a ON g.app_user_id = a.id
+    LEFT JOIN 
+        media_files f ON f.id = a.image_file_id
+    WHERE
+        role = 1 AND prayer_group_id = group_id;
+    RETURN;
+END;
+$$
+LANGUAGE plpgsql;
