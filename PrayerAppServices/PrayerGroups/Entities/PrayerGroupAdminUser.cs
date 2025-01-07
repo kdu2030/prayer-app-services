a2184369f4ed7f031d2f715659b7ce32460acedc
kdu2030 <kdu2030@gmail.com>
2025-01-07 11:20:40 -0500

    Add Prayer Group Admin User Entity

---
+++ PrayerAppServices/PrayerGroups/Entities/PrayerGroupAdminUser.cs
@@ -0,0 +1,14 @@
+﻿using PrayerAppServices.Files.Constants;
+using PrayerAppServices.PrayerGroups.Constants;
+
+namespace PrayerAppServices.PrayerGroups.Entities {
+    public class PrayerGroupAdminUser {
+        public int? Id { get; set; }
+        public string? FullName { get; set; }
+        public PrayerGroupRole? GroupRole { get; set; }
+        public int? ImageFileId { get; set; }
+        public string? FileName { get; set; }
+        public string? FileUrl { get; set; }
+        public FileType? FileType { get; set; }
+    }
+}
