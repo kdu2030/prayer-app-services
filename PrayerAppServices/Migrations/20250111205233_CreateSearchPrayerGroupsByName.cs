7ff7f6a382bb5998073a0bc6a3c9bfa72d1db28c
kdu2030 <kdu2030@gmail.com>
2025-01-11 15:54:52 -0500

    Add Search Prayer Group By Name Function

---
+++ PrayerAppServices/Migrations/20250111205233_CreateSearchPrayerGroupsByName.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrayerAppServices.Migrations {
+    /// <inheritdoc />
+    public partial class CreateSearchPrayerGroupsByName : Migration {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            string filePath = @"../PrayerAppServices/SQL/search_prayer_groups_by_name.sql";
+            migrationBuilder.Sql(File.ReadAllText(filePath));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS search_prayer_groups_by_name");
+        }
+    }
+}
