3c1590a70df04dacf9ad9285c1bc2180e837f9d5
kdu2030 <kdu2030@gmail.com>
2025-01-13 16:00:10 -0500

    Add Prayer Group Admin User List in Mock Data

---
+++ Tests/MockData/MockPrayerGroupData.cs
@@ -0,0 +1,31 @@
+﻿
+
+using PrayerAppServices.Files.Constants;
+using PrayerAppServices.PrayerGroups.Constants;
+using PrayerAppServices.PrayerGroups.Entities;
+
+namespace Tests.MockData {
+    public static class MockPrayerGroupData {
+        public static readonly IEnumerable<PrayerGroupAdminUser> prayerGroupAdminUsers = [
+            new PrayerGroupAdminUser {
+                Id = 1,
+                FullName = "Anakin Skywalker",
+                GroupRole = PrayerGroupRole.Admin,
+                ImageFileId = 1,
+                FileName = "anakin_skywalker.png",
+                FileType = FileType.Image,
+            },
+            new PrayerGroupAdminUser {
+                Id = 2,
+                FullName = "Obi Wan Kenobi"
+            },
+            new PrayerGroupAdminUser {
+                Id = 3,
+                FullName = "Count Dooku",
+                ImageFileId = 4,
+                FileName = "count_dooku.jpg",
+                FileType = FileType.Image,
+            }
+        ];
+    }
+}
