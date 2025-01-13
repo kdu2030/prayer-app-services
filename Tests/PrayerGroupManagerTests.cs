90b3434936eeef2f5e35d6383bfda0478c23ff7d
kdu2030 <kdu2030@gmail.com>
2025-01-13 11:07:39 -0500

    Add Create Prayer Group Test

---
+++ Tests/PrayerGroupManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using PrayerAppServices.PrayerGroups;
+using PrayerAppServices.PrayerGroups.Entities;
+using PrayerAppServices.PrayerGroups.Models;
+using PrayerAppServices.Users;
+
+namespace Tests {
+    public class PrayerGroupManagerTests {
+        [Test]
+        public void CreatePrayerGroup_GivenValidUser_CreatesPrayerGroup() {
+            string username = "abernard";
+            Mock<IUserManager> mockUserManager = new Mock<IUserManager>();
+            mockUserManager
+                .Setup(userManager => userManager.ExtractUsernameFromAuthHeader(It.IsAny<string>()))
+                .Returns(() => username);
+
+            CreatePrayerGroupResponse response = new CreatePrayerGroupResponse {
+                Id = 1,
+                GroupName = "Dunder Mifflin",
+                AdminUserId = 1,
+                AdminFullName = "Andy Bernard",
+            };
+            NewPrayerGroupRequest newPrayerGroup = new NewPrayerGroupRequest {
+                GroupName = "Dunder Mifflin"
+            };
+
+            Mock<IPrayerGroupRepository> mockRepository = new Mock<IPrayerGroupRepository>();
+            mockRepository
+                .Setup(mockRepository => mockRepository.CreatePrayerGroup(It.IsAny<string>(), It.IsAny<NewPrayerGroup>()))
+                .Returns(response);
+
+            IPrayerGroupManager prayerGroupManager = new PrayerGroupManager(mockRepository.Object, mockUserManager.Object);
+            PrayerGroupDetails details = prayerGroupManager.CreatePrayerGroup("mockToken", newPrayerGroup);
+
+            Assert.IsTrue((details.GroupName ?? "").Equals(newPrayerGroup.GroupName));
+            Assert.IsNotNull(details.Id);
+        }
+    }
+}
