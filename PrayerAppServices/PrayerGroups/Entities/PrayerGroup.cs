88ce942d003c649313825445e112f032c5ec679d
kdu2030 <kdu2030@gmail.com>
2025-01-13 17:40:00 -0500

    Make Prayer Group Name into a Varchar

--- PrayerAppServices/PrayerGroups/Entities/PrayerGroup.cs
+++ PrayerAppServices/PrayerGroups/Entities/PrayerGroup.cs
@@ -1,8 +1,11 @@
 ﻿using PrayerAppServices.Files.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PrayerAppServices.PrayerGroups.Entities {
     public class PrayerGroup {
         public int? Id { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
         public required string GroupName { get; set; }
         public string? Description { get; set; }
         public string? Rules { get; set; }
