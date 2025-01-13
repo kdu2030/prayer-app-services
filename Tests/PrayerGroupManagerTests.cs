f574dfb5af0d8ca216ba86f5bd3b9860ee243a95
kdu2030 <kdu2030@gmail.com>
2025-01-13 13:37:21 -0500

    Add Valid Image Test

--- Tests/PrayerGroupManagerTests.cs
+++ Tests/PrayerGroupManagerTests.cs
@@ -37,5 +37,45 @@ namespace Tests {
                 Assert.That(details.Id, Is.Not.Null);
             });
         }
+
+        [Test]
+        public void CreatePrayerGroup_GivenValidImage_AddImageToPrayerGroup() {
+            Mock<IUserManager> mockUserManager = new Mock<IUserManager>();
+            mockUserManager
+                .Setup(userManager => userManager.ExtractUsernameFromAuthHeader(It.IsAny<string>()))
+                .Returns(() => "mockUsername");
+            string mockFileName = "group_image.png";
+            string mockUrl = "http://127.0.0.1:5000/group_image.png";
+
+            CreatePrayerGroupResponse response = new CreatePrayerGroupResponse {
+                Id = 1,
+                GroupName = "Dunder Mifflin",
+                AdminUserId = 1,
+                AdminFullName = "Andy Bernard",
+                ImageFileId = 2,
+                GroupImageFileName = mockFileName,
+                GroupImageFileUrl = mockUrl
+            };
+
+            NewPrayerGroupRequest groupRequest = new NewPrayerGroupRequest {
+                GroupName = "Dunder Mifflin",
+                ImageFileId = 2,
+            };
+
+            Mock<IPrayerGroupRepository> mockRepository = new Mock<IPrayerGroupRepository>();
+            mockRepository
+                .Setup(mockRepository => mockRepository.CreatePrayerGroup(It.IsAny<string>(), It.IsAny<NewPrayerGroup>()))
+                .Returns(response);
+
+            IPrayerGroupManager prayerGroupManager = new PrayerGroupManager(mockRepository.Object, mockUserManager.Object);
+            PrayerGroupDetails details = prayerGroupManager.CreatePrayerGroup("mockToken", groupRequest);
+
+            Assert.Multiple(() => {
+                Assert.That(details.ImageFile?.Id, Is.EqualTo(2));
+                Assert.That(details.ImageFile?.FileName, Is.EqualTo(mockFileName));
+                Assert.That(details.ImageFile?.Url, Is.EqualTo(mockUrl));
+            });
+
+        }
     }
 }
