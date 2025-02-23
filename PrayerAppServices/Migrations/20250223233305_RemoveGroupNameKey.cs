e98f129768b10365a808f36c810400ac113ee8a4
kdu2030 <kdu2030@gmail.com>
2025-02-23 18:36:10 -0500

    Remove Group Name Alternate Key

---
+++ PrayerAppServices/Migrations/20250223233305_RemoveGroupNameKey.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrayerAppServices.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveGroupNameKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "ak_prayer_groups_group_name",
+                table: "prayer_groups");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "ak_prayer_groups_group_name",
+                table: "prayer_groups",
+                column: "group_name");
+        }
+    }
+}
---
+++ PrayerAppServices/Migrations/20250223233506_FixGroupNameUniqueConstraint.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrayerAppServices.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixGroupNameUniqueConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_prayer_groups_group_name",
+                table: "prayer_groups",
+                column: "group_name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_prayer_groups_group_name",
+                table: "prayer_groups");
+        }
+    }
+}
