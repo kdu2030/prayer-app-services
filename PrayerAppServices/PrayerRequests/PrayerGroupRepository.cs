2289b98b5a37a28bd323059d872889cc05b2edad
kdu2030 <kdu2030@gmail.com>
2025-04-13 20:54:58 -0400

    Add PrayerRequestModel

---
+++ PrayerAppServices/PrayerRequests/PrayerGroupRepository.cs
@@ -0,0 +1,10 @@
+﻿using PrayerAppServices.Data;
+
+namespace PrayerAppServices.PrayerRequests {
+    public class PrayerGroupRepository(AppDbContext dbContext) {
+        private readonly AppDbContext _dbContext = dbContext;
+
+
+
+    }
+}
