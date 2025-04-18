5df973118196f33328361a354438c9ab319beaf5
kdu2030 <kdu2030@gmail.com>
2025-04-17 21:52:06 -0400

    Add Test for Valid Prayer Request Call

---
+++ Tests/MockData/MockPrayerRequestData.cs
@@ -0,0 +1,25 @@
+﻿
+
+using PrayerAppServices.PrayerGroups.Constants;
+using PrayerAppServices.PrayerGroups.Entities;
+using PrayerAppServices.Users.Entities;
+
+namespace Tests.MockData {
+    public static class MockPrayerRequestData {
+        public static readonly PrayerGroupUser MockPrayerGroupUser = new PrayerGroupUser {
+            Id = 1,
+            AppUser = new AppUser {
+                Id = 2,
+                FullName = "Anakin Skywalker",
+            },
+            Role = PrayerGroupRole.Member,
+            PrayerGroup = new PrayerGroup {
+                Id = 3,
+                GroupName = "IMB",
+                Color = 65280,
+                Description = "Missionary organization",
+                Rules = "No explicit language",
+            },
+        };
+    }
+}
