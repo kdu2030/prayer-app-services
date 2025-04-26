b74ed6c3f403ce92aa2766e8aaf30f957b323315
kdu2030 <kdu2030@gmail.com>
2025-04-26 14:28:15 -0400

    Rename Sort Fields

--- PrayerAppServices/PrayerRequests/Constants/PrayerRequestSortFields.cs
+++ PrayerAppServices/PrayerRequests/Constants/PrayerRequestSortFields.cs
@@ -1,8 +1,8 @@
 ﻿namespace PrayerAppServices.PrayerRequests.Constants {
     public static class PrayerRequestSortFields {
         public static readonly string CreatedAt = "CreatedDate";
-        public static readonly string MostLiked = "MostLiked";
-        public static readonly string MostCommented = "MostCommented";
-        public static readonly string MostPrayed = "MostPrayed";
+        public static readonly string LikeCount = "LikeCount";
+        public static readonly string CommentCount = "CommentCount";
+        public static readonly string PrayedCount = "PrayedCount";
     }
 }
