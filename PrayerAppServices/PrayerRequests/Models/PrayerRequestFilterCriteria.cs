7a0962be3b41be2ea34630b19d2de278177dba91
kdu2030 <kdu2030@gmail.com>
2025-04-26 14:21:13 -0400

    Add Sort Order

--- PrayerAppServices/PrayerRequests/Models/PrayerRequestFilterCriteria.cs
+++ PrayerAppServices/PrayerRequests/Models/PrayerRequestFilterCriteria.cs
@@ -1,9 +1,12 @@
-﻿namespace PrayerAppServices.PrayerRequests.Models {
+﻿using PrayerAppServices.Common.Sorting;
+
+
+namespace PrayerAppServices.PrayerRequests.Models {
     public class PrayerRequestFilterCriteria {
         public int[]? PrayerGroupIds { get; set; }
         public int[]? CreatorUserIds { get; set; }
         public int? PageIndex { get; set; }
         public int? PageSize { get; set; }
-
+        public required SortConfig<string> SortConfig { get; set; }
     }
 }
