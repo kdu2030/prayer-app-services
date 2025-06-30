832c3a5d48d9beb46b64ac3e299a44699595b797
kdu2030 <kdu2030@gmail.com>
2025-06-29 21:33:58 -0400

    Add Add/Remove Like Routes to Prayer Request Controller Interface

--- PrayerAppServices/PrayerRequests/IPrayerRequestController.cs
+++ PrayerAppServices/PrayerRequests/IPrayerRequestController.cs
@@ -5,5 +5,7 @@ namespace PrayerAppServices.PrayerRequests {
     public interface IPrayerRequestController {
         Task<ActionResult> CreatePrayerRequestAsync(int prayerGroupId, PrayerRequestCreateRequest createRequest, CancellationToken token);
         Task<ActionResult<PrayerRequestGetResponse>> GetPrayerRequestsAsync(PrayerRequestFilterRequest request, CancellationToken token);
+        Task<ActionResult> AddPrayerRequestLikeAsync(int prayerRequestId, [FromQuery] int userId, CancellationToken token);
+        Task<ActionResult> RemovePrayerRequestLikeAsync(int prayerRequestId, [FromQuery] int userId, CancellationToken token);
     }
 }
