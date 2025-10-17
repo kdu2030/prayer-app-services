826a9da1d6bac156c6b5d04ac6bffaca995d9a58
kdu2030 <kdu2030@gmail.com>
2025-10-17 08:21:30 -0400

    Cleanup Imports

--- PrayerAppServices/PrayerGroups/Models/PrayerGroupUsersResponse.cs
+++ PrayerAppServices/PrayerGroups/Models/PrayerGroupUsersResponse.cs
@@ -1,7 +1,7 @@
-﻿using PrayerAppServices.Users.Models;
-
-namespace PrayerAppServices.PrayerGroups.Models {
-    public class PrayerGroupUsersResponse {
+﻿namespace PrayerAppServices.PrayerGroups.Models
+{
+    public class PrayerGroupUsersResponse
+    {
         public IEnumerable<PrayerGroupUserSummary>? Users { get; set; }
     }
 }
