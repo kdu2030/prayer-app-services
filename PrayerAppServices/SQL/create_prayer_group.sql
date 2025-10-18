1d5ababb5e9b57848a875fc524615b25379f831d
kdu2030 <kdu2030@gmail.com>
2025-10-17 20:12:17 -0400

    Update Create Prayer Group Stored Function

--- PrayerAppServices/SQL/create_prayer_group.sql
+++ PrayerAppServices/SQL/create_prayer_group.sql
@@ -1,25 +1,25 @@
 DROP FUNCTION IF EXISTS create_prayer_group;
 
 CREATE OR REPLACE FUNCTION create_prayer_group(
-    username VARCHAR(255),
+    creator_user_id INT,
     new_group_name VARCHAR(255),
     group_description TEXT,
     group_rules TEXT,
-    group_color INT,
-    group_image_file_id INT,
-    group_banner_image_file_id INT
+    group_visibility INT,
+    group_avatar_file_id INT,
+    group_banner_file_id INT
 ) RETURNS TABLE (
-    id INT,
+    prayer_group_id INT,
     group_name VARCHAR(255),
     description TEXT,
     rules TEXT,
-    color INT,
-    image_file_id INT,
-    group_image_file_name VARCHAR(255),
-    group_image_file_url VARCHAR(255),
-    banner_image_file_id INT,
-    banner_image_file_name VARCHAR(255),
-    banner_image_file_url VARCHAR(255),
+    visibility_level INT,
+    avatar_file_id INT,
+    group_avatar_file_name VARCHAR(255),
+    group_avatar_file_url VARCHAR(255),
+    banner_file_id INT,
+    group_banner_file_name VARCHAR(255),
+    group_banner_file_url VARCHAR(255),
     admin_user_id INT,
     admin_full_name VARCHAR(255),
     admin_image_file_id INT,
@@ -28,104 +28,104 @@ CREATE OR REPLACE FUNCTION create_prayer_group(
 )
 AS
 $$
-DECLARE 
+DECLARE
         new_group_id INT;
 BEGIN
     DROP TABLE IF EXISTS temp_admin_user;
     DROP TABLE IF EXISTS temp_relevant_files;
-    
+
     CREATE TEMPORARY TABLE temp_admin_user (
-        id INT,
+        app_user_id INT,
         full_name VARCHAR(255),
         image_file_id INT
     );
 
     CREATE TEMPORARY TABLE temp_relevant_files (
-        id INT,
+        media_file_id INT,
         file_name VARCHAR(255),
-        url VARCHAR(255),
+        file_url VARCHAR(255),
         file_type INT
     );
 
-    INSERT INTO 
-        temp_admin_user (id, full_name, image_file_id)
+    INSERT INTO
+        temp_admin_user (app_user_id, full_name, image_file_id)
     SELECT
         u.id,
         full_name,
         u.image_file_id
     FROM
         asp_net_users u
-    WHERE user_name = username;
+    WHERE u.id = creator_user_id;
 
     INSERT INTO
-        temp_relevant_files (id, file_name, url, file_type)
+        temp_relevant_files (media_file_id, file_name, file_url, file_type)
     SELECT
-        f.id,
+        f.media_file_id,
         f.file_name,
-        f.url,
+        f.file_url,
         f.file_type
     FROM media_files f, temp_admin_user a
-    WHERE f.id IN (a.image_file_id, group_image_file_id, group_banner_image_file_id);
+    WHERE f.media_file_id IN (a.image_file_id, group_avatar_file_id, group_banner_file_id);
 
     IF NOT EXISTS (SELECT 1 FROM temp_admin_user)
     THEN
         RAISE EXCEPTION 'User does not exist.';
     END IF;
 
-    IF group_image_file_id IS NOT NULL AND NOT EXISTS (
-        SELECT 1 FROM temp_relevant_files f 
-        WHERE f.id = group_image_file_id AND file_type = 1
-    )
-    THEN
-        RAISE EXCEPTION 'Image file for group not found or file is not an image.';
-    END IF;
-
-    IF group_banner_image_file_id IS NOT NULL AND NOT EXISTS (
+    IF group_avatar_file_id IS NOT NULL AND NOT EXISTS (
         SELECT 1 FROM temp_relevant_files f
-        WHERE f.id = group_banner_image_file_id AND file_type = 1
+        WHERE f.media_file_id = group_avatar_file_id AND file_type = 1
     )
     THEN
-        RAISE EXCEPTION 'Banner image file for group not found or file is not an image.';
+        RAISE EXCEPTION 'Avatar image for group not found or file is not an image.';
+    END IF;
+
+    IF group_banner_file_id IS NOT NULL AND NOT EXISTS (
+        SELECT 1 FROM temp_relevant_files f
+        WHERE f.media_file_id = group_banner_file_id AND file_type = 1
+    )
+    THEN
+        RAISE EXCEPTION 'Banner image for group not found or file is not an image.';
     END IF;
 
 
     INSERT INTO
-        prayer_groups (group_name, description, rules, color, image_file_id, banner_image_file_id)
+        prayer_groups (group_name, description, rules, visibility_level, avatar_file_id, banner_file_id)
     VALUES
-        (new_group_name, group_description, group_rules, group_color, group_image_file_id, group_banner_image_file_id)
-    RETURNING prayer_groups.id INTO new_group_id;
+        (new_group_name, group_description, group_rules, group_visibility, group_avatar_file_id, group_banner_file_id)
+    RETURNING prayer_groups.prayer_group_id INTO new_group_id;
 
     INSERT INTO
-        prayer_group_users (prayer_group_id, app_user_id, role)
+        prayer_group_users (prayer_group_id, app_user_id, prayer_group_role)
     SELECT
         new_group_id,
-        a.id,
+        a.app_user_id,
         1
     FROM
         temp_admin_user a;
 
-    RETURN QUERY 
-        SELECT 
-            new_group_id, 
-            new_group_name, 
-            group_description, 
+    RETURN QUERY
+        SELECT
+            new_group_id,
+            new_group_name,
+            group_description,
             group_rules,
-            group_color,
-            group_image_file_id,
+            group_visibility,
+            group_avatar_file_id,
             f2.file_name,
-            f2.url,
-            group_banner_image_file_id,
+            f2.file_url,
+            group_banner_file_id,
             f3.file_name,
-            f3.url,
-            a.id,
+            f3.file_url,
+            a.app_user_id,
             a.full_name,
             a.image_file_id,
             f1.file_name,
-            f1.url
-        FROM temp_admin_user a 
-        LEFT JOIN temp_relevant_files f1 ON a.image_file_id = f1.id
-        LEFT JOIN temp_relevant_files f2 ON f2.id = group_image_file_id
-        LEFT JOIN temp_relevant_files f3 ON f3.id = group_banner_image_file_id;
+            f1.file_url
+        FROM temp_admin_user a
+        LEFT JOIN temp_relevant_files f1 ON a.image_file_id = f1.media_file_id
+        LEFT JOIN temp_relevant_files f2 ON f2.media_file_id = group_avatar_file_id
+        LEFT JOIN temp_relevant_files f3 ON f3.media_file_id = group_banner_file_id;
     DROP TABLE IF EXISTS temp_admin_user;
     DROP TABLE IF EXISTS temp_relevant_files;
     RETURN;
